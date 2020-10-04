243f408ad8cb58453476af6637715d251afe4974
unknown <yoshi7591@naver.com>
2020-10-04 17:41:51 +0900

    :wq!

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import config from './config';
 
 const app = express();
+const { MONGO_URI } = config;
 
-app.get('/');
+mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(()=> console.log("MongoDB Connected Successfully!"))
+    .catch((e) => console.log(e));
+
+app.get("/");
 
 export default app;
