ae1f84996932fee4105113c87b391e3eb9d747b1
unknown <yoshi7591@naver.com>
2020-10-25 08:32:17 +0900

    commit

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import config from './config';
+import config from './config/index.js';
 
 const app = express();
 const { MONGO_URI } = config;
